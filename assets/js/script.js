67216b488cb76e79adb81449e27828c50dc81647
Thomas <tmrtw8@gmail.com>
2023-04-10 18:17:59 -0600

    jsandcsshrefsfixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ var timeDisplayEl = $('time-display');
 
 
 $(function () {
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -39,15 +41,18 @@ $(function () {
 
   // TODO: Add code to display the current date in the header of the page.
 
-  function displayTime() {
-    var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
-    timeDisplayEl.text(rightNow);
-  }
+
   
-  displayTime();
-  setInterval(displayTime, 1000);
-  
-  printProjectData();
+
   
 });
 
+function displayTime() {
+  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEl.text(rightNow);
+}
+
+displayTime();
+setInterval(displayTime, 1000);
+
+// printProjectData();
